5559cba87ddbe3cb4fd3b14d7977459785fab399
DJPretzel-bit64 <dylanjesse18@gmail.com>
2022-07-01 20:30:06 +1000

    removed comment

--- src/main/java/net/dylan/penguins/PenguinClient.java
+++ src/main/java/net/dylan/penguins/PenguinClient.java
@@ -2,7 +2,6 @@ package net.dylan.penguins;
 
 import net.fabricmc.api.ClientModInitializer;
 
-// Very important comment
 public class PenguinClient implements ClientModInitializer  {
     @Override
     public void onInitializeClient() {

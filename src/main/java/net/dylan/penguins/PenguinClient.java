a0a51476a94c62ad5fd69b992a459f2c641d9719
DJPretzel-bit64 <dylanjesse18@gmail.com>
2022-07-01 20:27:08 +1000

    didn't upload

---
+++ src/main/java/net/dylan/penguins/PenguinClient.java
@@ -0,0 +1,11 @@
+package net.dylan.penguins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+// Very important comment
+public class PenguinClient implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dylan/penguins/Penguins.java
@@ -0,0 +1,14 @@
+package net.dylan.penguins;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Penguins implements ModInitializer {
+	public static final String MOD_ID = "penguins";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}

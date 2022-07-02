628977cf47e7fe17d1f8ef08f388da417fbbdc96
DJPretzel-bit64 <dylanjesse18@gmail.com>
2022-07-02 20:03:29 +1000

    added penguin_gun

--- src/main/java/net/dylan/penguins/item/ModItems.java
+++ src/main/java/net/dylan/penguins/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.dylan.penguins.item;
 
 import net.dylan.penguins.Penguins;
+import net.dylan.penguins.item.custom.PenguinGunItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -15,6 +16,9 @@ public class ModItems {
 
     public static final Item PENGUIN_HEAD = registerItem("penguin_head",
             new Item(new FabricItemSettings().group(ModItemGroup.PENGUIN_STUFFS)));
+
+    public static final Item PENGUIN_GUN = registerItem("penguin_gun",
+            new PenguinGunItem(new FabricItemSettings().group(ModItemGroup.PENGUIN_STUFFS).maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Penguins.MOD_ID, name), item);
